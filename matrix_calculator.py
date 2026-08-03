259b83ee50410d477c96cf2aaee7340068ed4a65
Ireoluwa_Mike <mikethestemist@gmail.com>
2026-08-03 10:41:15 +0100

    make utility and checking features

--- matrix_calculator.py
+++ matrix_calculator.py
@@ -18,15 +18,49 @@
 matrix_sample = [[1, 2, 3], 
                  [4, 5, 6], 
                  [7, 8, 9]]
+bad_matrix_sample = [[1, 2,], 
+                 [4, 5, 6], 
+                 [7, 8, 9]]
 
-print(matrix_sample)
+# print(matrix_sample)
 
 # -----   Utilities and Checks   ----- #
-def is_square_matrix(m1, m2): 
-  pass 
+def column_count_is_uniform(m): 
+  columns = set()
+  for row in m: 
+    columns.add(len(row))  
+  if len(columns) == 1: 
+    return True
+  elif len(columns) != 1: 
+    print(f'{m} has inconsitent column entries.')
+    return False
+
+def is_square_matrix(m): 
+  rows = len(m)
+  columns = set()
+  for row in m: 
+    columns.add(len(row))  
+  if column_count_is_uniform(m) and rows == sorted(columns)[0]: 
+    return True
+  elif rows != sorted(columns)[0]: 
+    return False
 
 def can_multiply(m1, m2): 
-  pass 
+  m1_row_count = len(m1[0])
+  m2_column_count = len(m1)
+  return m1_row_count == m2_column_count and column_count_is_uniform(m1) and column_count_is_uniform(m2)
+
+def make_identity_matrix(square_size): 
+  m = []
+  for i in range(square_size): 
+    row = []
+    for j in range(square_size): 
+      if i == j: 
+        row.append(1)
+      else: 
+        row.append(0)
+    m.append(row)
+  return m
 
 # -----   Basic Operations   ----- #
 def add_matrices(m1, m2): 
