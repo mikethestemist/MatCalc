b7f300aa49bd02a806c4438d6da4e1d01ab45c0c
Ireoluwa_Mike <mikethestemist@gmail.com>
2026-09-10 21:52:38 +0100

    optimise: include types for return and argument types and add docstrings

--- MatCalc.py
+++ MatCalc.py
@@ -11,7 +11,8 @@ bad_sample_matrix = [[1, 2,],
 # print(matrix_sample)
 
 # -----   User Validations   ----- #
-def is_valid_num(num): 
+def is_valid_num(num: int | float | list) -> bool: 
+  """Takes in a value or a list of values and returns a single boolean stating whether or not the value or the list of values is a valid integer or float — number."""
   if type(num) == list: 
     validity_set = set()
     for x in num:
@@ -23,7 +24,8 @@ def is_valid_num(num):
 
 
 # -----   Utilities and Checks   ----- #
-def is_valid_matrix(m):
+def is_valid_matrix(m: list[list[int | float]]) -> bool:
+  """Takes a two dimensional list of numbers to represent a matrix and returns a single boolean confirming or denying that it is a valid matrix. It checks for consistent column count and validates that all entries are numbers. It also prints out the matrix and a message stating the error."""
   if m == None: 
     return False
   elif not type(m) == list:
@@ -43,17 +45,19 @@ def is_valid_matrix(m):
       print(f'{m} has inconsitent column entries.')
       return False
 
-def get_order(m): 
+def get_order(m: list[list[int | float]]) -> tuple[int, int]: 
+  """It takes in a two dimensional list of numbers, verifies it's a valid matrix and returns the order as a tuple. The first value is the number of rows, the m, and the second value the number of columns or items per row, the n."""
   if is_valid_matrix(m): 
-    """m: rows, n: columns / entries per row"""
     m_rows = len(m)
     n_cols = len(m[0])
     return (m_rows, n_cols)
 
-def has_same_order(m1, m2): 
+def has_same_order(m1: list[list[int | float]], m2: list[list[int | float]]) -> bool: 
+  """Takes in two two dimensional list of numbers and returns a boolean showing whether they were of the same order — had the same number of rows and columns, or not."""
   return get_order(m1) == get_order(m2)
 
-def is_square_matrix(m): 
+def is_square_matrix(m: list[list[int | float]]) -> bool: 
+  """Takes in a two dimensional list of numbers and returns a boolean stating whether or not the columns of the matrix have the same count as the rows."""
   rows = len(m)
   columns = set()
   for row in m: 
@@ -63,14 +67,16 @@ def is_square_matrix(m):
   elif rows != sorted(columns)[0]: 
     return False
 
-def can_multiply(m1, m2): 
+def can_multiply(m1: list[list[int | float]], m2: list[list[int | float]]) -> bool: 
+  """Takes in two lists of lists and returns a boolean whether or not the column count, m, of the first matrix is the same as the row count, n of the second matrix."""
   m1_col_count = get_order(m1)[1]
   m2_row_count = get_order(m2)[0]
   return m1_col_count == m2_row_count
 
-def generate_identity_matrix(square_size): 
-  if not is_valid_num(square_size):
-    print('Invalid square size for identity matrix generation.')
+def generate_identity_matrix(square_size: int) -> list[list[int]]: 
+  """It generates aa matrix — a two dimensional list of numbers — with its leading diagonal being 1 and all other values being zero."""
+  if type(square_size) == int:
+    print('Invalid whole number for the square size of the identity matrix.')
     return None
   m = []
   for i in range(square_size): 
@@ -83,21 +89,28 @@ def generate_identity_matrix(square_size):
     m.append(row)
   return m
 
-def gen_rand(m_row, n_row, x_range): 
-  vals = [m_row, n_row, x_range]
+def gen_rand(m_row: int, n_col: int, x_range: int) -> list[list[int | float]]: 
+  """"Takes in three values. The first being the number of rows you want to generate, the second being the number of columns and the third being the max value that could possibly occure. It preoccupies the matrix with random variables between zero and the max value specified."""
+
+  # TODO: ensure that the m_row and n_col are valid integers
+  # TODO: make option for a lower bound and upper bound
+  # TODO: make the range able to accept a float and return floats 
+
+  vals = [m_row, n_col, x_range]
   if not is_valid_num(vals):
     print('Invalid number as input for random matrix generation.')
     return None
   m = []
   for i in range(m_row): 
     row = []
-    for j in range(n_row): 
+    for j in range(n_col): 
       row.append(randint(0, x_range))    
     m.append(row)
   return m
 
 # -----   Basic Operations   ----- #
-def add_matrices(m1, m2): 
+def add_matrices(m1: list[list[int | float]], m2: list[list[int | float]])-> list[list[int | float]]: 
+  """It verifies that the matrices — two dimensional list of lists — is valid and then returns a matrix having the entities at each row by column (m x n) position summed up. It works for two matrices at a time."""
   if has_same_order(m1, m2): 
     result = []
     for i in zip(m1, m2): 
@@ -109,7 +122,8 @@ def add_matrices(m1, m2):
   else: 
     print(m1, 'and', m2, 'do not have the same order.')
 
-def subtract_matrices(m1, m2): 
+def subtract_matrices(m1: list[list[int | float]], m2: list[list[int | float]]) -> list[list[int | float]]: 
+  """It verifies that the matrices — two dimensional list of lists — is valid and then returns a matrix having the entities of the second matrix subtracted from the first at each row by column (m x n) position. It works for two matrices at a time."""
   if has_same_order(m1, m2): 
     result = []
     for i in zip(m1, m2): 
@@ -121,8 +135,11 @@ def subtract_matrices(m1, m2):
   else: 
     print(m1, 'and', m2, 'do not have the same order.')
 
-def multiply(m1_or_scalar, m2): 
-  if type(m1_or_scalar) == int or type(m1_or_scalar) == float: 
+def multiply(m1_or_scalar: int | float | list[list[int | float]], m2: list[list[int | float]])-> list[list[int | float]]: 
+  """It takes in two values. The first either being a scalar single digit, or being a two dimensional list of numbers like the second value. If the first value is a scalar, it multiplies all the entries of the other matrix with that value. If the first value were to be a matrix, it gives the sum-product for each entry moving through the rows for the first matrix and the columns of the second matrix."""
+
+  # TODO: Debug multiply function
+  if type(m1_or_scalar) in [int, float]: 
     if is_valid_matrix(m2): 
       s = m1_or_scalar
       m = m2
@@ -150,7 +167,8 @@ def multiply(m1_or_scalar, m2):
 # print(multiply_matrices(matrix_sample, [[j * 3 for j in i] for i in generate_identity_matrix(3)]))
 
 # -----   Matrix Functions   ----- #
-def transpose(m): 
+def transpose(m: list[list[int | float]]) -> list[list[int | float]]: 
+  """It takes the rows of the matrixs and returns a new matrix with those rows as columns. It's anchor is the first entry — it takes the first row and makes it the first column while making the first entry the same."""
   if is_valid_matrix(m):
     m_new = []
     for i in range(len(m)): 
@@ -160,7 +178,10 @@ def transpose(m):
       m_new.append(row)
     return m_new
 
-def minor(m_row, n_column, matrix): 
+def minor(m_row: int, n_column: int, matrix: list[list[int | float]]) -> list[list[int | float]]: 
+  """It takes in a zero-indexed row and column number and returns a two dimensional list of numbers excluding that row and column entirely."""
+  # TODO: Do a number validation for m_row and n_column 
+  # TODO: Ensure that the m_row and n_column are valid locations in the matrix 
   if is_valid_matrix(matrix): 
     minor_matrix = []
     for i in range(len(matrix)): 
@@ -172,7 +193,8 @@ def minor(m_row, n_column, matrix):
         minor_matrix.append(row)
     return minor_matrix
   
-def cofactor(m): 
+def cofactor(m: list[list[int | float]]) -> list[list[int | float]]: 
+  """It cycles through the matrix, takes the determinant of the minor of that entry and multiplies it with its respective sign, while not multiplying it with the entry itself, and returns the resulting matrix."""
   if is_square_matrix(m):
     matrix = []
     for i in range(len(m)): 
@@ -182,7 +204,8 @@ def cofactor(m):
       matrix.append(row)
     return matrix
 
-def determinant(m):
+def determinant(m: list[list[int | float]]) -> list[list[int | float]]:
+  """It cycles through the matrix, takes the determinant of the minor of that entry, multiplies it with its respective sign and the entry itself, and returns the resulting matrix."""
   if is_square_matrix(m):
     if get_order(m) == (1, 1): 
       return m[0][0]
@@ -196,12 +219,15 @@ def determinant(m):
   else: 
     print(f"{m} isn't a square matrix, the determinant and cofactor of this matrix cannot be found.")
 
-def adjunt(m): 
+def adjunt(m: list[list[int | float]]) -> list[list[int | float]]: 
+  """Returns the transpose of the cofactor of the accepted matrix"""
   return transpose(cofactor(m))
 
-def inverse(m): 
+def inverse(m: list[list[int | float]]) -> list[list[int | float]]: 
+  """Returns the product of the inverse of the determinant and the transponse of the cofactor — adjunt — of the accepted matrix"""
   return multiply(determinant(m) ** (-1), adjunt(m))
 
 # -----   Custom Calculation Functions   ----- #
-def solve_linear_system(A, P): 
+def solve_linear_system(A: list[list[int | float]], P: list[list[int | float]]) -> list[list[int | float]]: 
+  """Takes in the matrix of the coefficients of the unknowns at the left-hand-side of a simultaneous linear equation as the first argument and takes in the answers to the equations as a matrix as the second argument and returns the solution to the system of linear equations.."""
   return multiply(inverse(A), P)
--- TODO.md
+++ TODO.md
@@ -3,17 +3,20 @@
 ### Urgent + Important
 - [X] Correct m rows and n columns 
 
+- [ ] Debug multiply function
 ### Urgent + Not Important
-- [ ] Validate for user input being numbers — make a function that checks if int / float
+- [X] Validate for user input being numbers — make a function that checks if int / float
 
 ### Not Urgent + Important
 - [ ] Show the parameter type for all functions — list[list[int]]
-- [ ] Make descriptive comments for these functions
+- [x] Make descriptive comments for these functions
 - [ ] Write test scripts for the matrix
+- [ ] Make functions be able to accept more than two matrices when possible
 
 ### Not Urgent + Not Important
 - [ ] Modify `gen_rand()` to take in optional upper and lower boundaries
 - [ ] Shorten function names for matrix ops functions, e.g., inverse -> inv, etc.
 - [ ] Modularise code into segments: Basic operations, Utils, Matrix ops., etc.
+- [ ] Let matrix be printed on the same tab level
 
 
